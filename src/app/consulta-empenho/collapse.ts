3414d8e6788bfc6ab1596ccf7dfe7cfd32ddb442
akmerejf <akmerejf@gmail.com>
2017-08-10 10:11:02 -0300

    ajustes no tempalte

--- src/app/consulta-empenho/collapse.ts
+++ src/app/consulta-empenho/collapse.ts
@@ -9,9 +9,9 @@ import {BrowserModule} from '@angular/platform-browser'
       <div class="panel-heading"><span class="glyphicon glyphicon-arrow-down" *ngIf="!visible" aria-hidden="true" (click)="visible = !visible"></span><span class="glyphicon glyphicon-arrow-up" *ngIf="visible" aria-hidden="true" (click)="visible = !visible"></span> {{title}}</div>
       
       <div class="panel-body" *ngIf="visible">
-        <div class="row">
+        
           <ng-content></ng-content>
-        </div>
+        
       </div>
     </div>
  </div>
